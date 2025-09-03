adfbba5572a84a6fc4bdb6664bbee1d7c9a49f3b
SeoBoGyeong <163421322+seo-bo@users.noreply.github.com>
2025-09-03 20:58:11 +0900

    [Bronze V] Title: 새싹, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/25083. 새싹/새싹.cc
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+
+int main(void)
+{
+	cin.tie(0)->sync_with_stdio(0);
+	cout << "         ,r\'\"7\n";
+	cout << "r`-_   ,\'  ,/" << '\n';
+	cout << " \\. \". L_r\'" << '\n';
+	cout << "   \`~\\/" << '\n';
+	cout << "      |\n";
+	cout << "      |\n";
+	return 0;
+}
