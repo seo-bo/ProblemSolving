a8733e75e23b3df08d0da19e166e8d972e225b60
SeoBoGyeong <163421322+seo-bo@users.noreply.github.com>
2025-09-03 21:00:23 +0900

    [Bronze V] Title: A/B, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/1008. A／B/A／B.cc
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+
+int main(void)
+{
+	cin.tie(0)->sync_with_stdio(0);
+	double a = 0, b = 0;
+	cin >> a >> b;
+	cout << fixed << setprecision(13) << a / b;
+	return 0;
+}
