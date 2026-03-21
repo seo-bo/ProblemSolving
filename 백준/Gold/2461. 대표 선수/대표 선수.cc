d99fe76cea63166426e6d4f69c693bc4c8aa93e0
SeoBoGyeong <fgxhmd@kumoh.ac.kr>
2026-03-21 15:50:51 +0900

    [Gold II] Title: 대표 선수, Time: 184 ms, Memory: 14500 KB -BaekjoonHub

--- 백준/Gold/2461. 대표 선수/대표 선수.cc
+++ 백준/Gold/2461. 대표 선수/대표 선수.cc
@@ -1,12 +1,42 @@
-<html>
-<head><title>502 Bad Gateway</title></head>
-<body>
-<center><h1>502 Bad Gateway</h1></center>
-</body>
-</html>
-<!-- a padding to disable MSIE and Chrome friendly error page -->
-<!-- a padding to disable MSIE and Chrome friendly error page -->
-<!-- a padding to disable MSIE and Chrome friendly error page -->
-<!-- a padding to disable MSIE and Chrome friendly error page -->
-<!-- a padding to disable MSIE and Chrome friendly error page -->
-<!-- a padding to disable MSIE and Chrome friendly error page -->
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+typedef pair<int, int>pii;
+
+int main(void)
+{
+	cin.tie(0)->sync_with_stdio(0);
+	int n = 0, m = 0;
+	cin >> n >> m;
+	vector<pii>v;
+	for (int i = 1; i <= n; ++i)
+	{
+		for (int j = 0; j < m; ++j)
+		{
+			int a = 0;
+			cin >> a;
+			v.push_back(make_pair(a, i));
+		}
+	}
+	sort(v.begin(), v.end());
+	vector<int>cnt(n + 1);
+	int ans = INT_MAX;
+	for (int left = 0, now = 0, right = 0; right < n * m; ++right)
+	{
+		auto [co, ve] = v[right];
+		if (++cnt[ve] == 1)
+		{
+			now++;
+		}
+		while (cnt[v[left].second] - 1)
+		{
+			--cnt[v[left++].second];
+		}
+		if (now == n)
+		{
+			ans = min(ans, v[right].first - v[left].first);
+		}
+	}
+	cout << ans;
+	return 0;
+}
