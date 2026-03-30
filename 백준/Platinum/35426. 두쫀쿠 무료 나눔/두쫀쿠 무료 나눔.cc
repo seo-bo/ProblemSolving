46eacb1957f89ff639eb436132c74ad952793706
SeoBoGyeong <fgxhmd@kumoh.ac.kr>
2026-03-30 10:36:06 +0900

    [Platinum IV] Title: 두쫀쿠 무료 나눔, Time: 272 ms, Memory: 33476 KB, Score: 100 point -BaekjoonHub

---
+++ 백준/Platinum/35426. 두쫀쿠 무료 나눔/두쫀쿠 무료 나눔.cc
@@ -0,0 +1,60 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+typedef pair<ll, ll>pll;
+typedef tuple<ll, ll, ll>tp;
+
+int main(void)
+{
+	cin.tie(0)->sync_with_stdio(0);
+	int n = 0, m = 0;
+	cin >> n >> m;
+	vector<vector<pll>>graph(n + 3), rev(n + 3);
+	for (int i = 0; i < m; ++i)
+	{
+		ll a = 0, b = 0, c = 0;
+		cin >> a >> b >> c;
+		if (c & 1)
+		{
+			c = (b - a + 1) * c;
+			graph[a].push_back(make_pair(b, c));
+			rev[b].push_back(make_pair(a - 1, c));
+		}
+	}
+	vector<vector<ll>>dist(n + 1, vector<ll>(2, LLONG_MAX));
+	dist[0][0] = 0;
+	priority_queue<tp, vector<tp>, greater<tp>>pq;
+	pq.push(make_tuple(0, 0, 0));
+	while (!pq.empty())
+	{
+		auto [co, cnt, ver] = pq.top();
+		pq.pop();
+		if (dist[ver][cnt] < co)
+		{
+			continue;
+		}
+		for (auto& [nv, nc] : rev[ver])
+		{
+			if (dist[nv][0] > nc + co)
+			{
+				dist[nv][0] = nc + co;
+				pq.push(make_tuple(nc + co, 0, nv));
+			}
+		}
+		for (auto& [nv, nc] : graph[ver + 1])
+		{
+			if (dist[nv][1] > nc + co)
+			{
+				dist[nv][1] = nc + co;
+				pq.push(make_tuple(nc + co, 1, nv));
+			}
+		}
+	}
+	if (dist[n][1] == LLONG_MAX)
+	{
+		cout << "Dujjonku so expensive T.T";
+		return 0;
+	}
+	cout << dist[n][1];
+	return 0;
+}
