1bd3aacdbd927e94712959e8c1b1af82615586f2
SeoBoGyeong <fgxhmd@kumoh.ac.kr>
2026-04-09 10:53:13 +0900

    [Gold III] Title: 토끼의 이동, Time: 4 ms, Memory: 2084 KB -BaekjoonHub

--- 백준/Gold/3101. 토끼의 이동/토끼의 이동.cc
+++ 백준/Gold/3101. 토끼의 이동/토끼의 이동.cc
@@ -7,30 +7,32 @@ int main(void)
 	cin.tie(0)->sync_with_stdio(0);
 	int n = 0, k = 0;
 	cin >> n >> k;
-	string str, base = "UDRL";
-	cin >> str;
+	string base = "UDRL";
 	vector<vector<int>>dir = { {-1,0},{1,0},{0,1},{0,-1} };
-	vector<ll>sum(2 * n + 1);
 	ll ans = 1;
-	for (int i = 2, p = 1; i <= 2 * n; ++i)
-	{
-		sum[i] += sum[i - 1] + p;
-		(i >= n + 1) ? p-- : p++;
-	}
+	auto cal = [&](int a)
+		{
+			if (a <= n + 1)
+			{
+				return (ll)a * (a - 1) / 2;
+			}
+			return (ll)n * n - (ll)(n * 2 - a) * (n * 2 - a + 1) / 2;
+		};
 	int x = 1, y = 1;
-	for (auto& i : str)
+	for (int i = 0; i < k; ++i)
 	{
+		char a;
+		cin >> a;
 		for (int j = 0; j < 4; ++j)
 		{
-			if (base[j] == i)
+			if (base[j] == a)
 			{
-				x += dir[j][0];
-				y += dir[j][1];
+				x += dir[j][0], y += dir[j][1];
 				break;
 			}
 		}
 		int pos = x + y, co = max(0, pos - n - 1);
-		ans += sum[pos - 1] + ((pos % 2) ? x - co : y - co);
+		ans += cal(pos - 1) + ((pos % 2) ? x - co : y - co);
 	}
 	cout << ans;
 	return 0;
